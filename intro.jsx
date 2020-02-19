8a46edbc6bf6326af98b39027455eb3e68251a41
dcorn068 <daniel@raventelemetry.com>
2020-02-19 17:13:17 -0500

    init shrotcuts-challenge

---
+++ intro.jsx
@@ -0,0 +1,30 @@
+//
+//      🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸
+//      🤸🤸🤸 Shortcuts  Challenge 🤸🤸🤸
+//      🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸🤸
+//
+//
+//
+//            ------ DA RULES ------
+//
+//
+//
+//      0. Install the extension "Sublime Text Keymap and Settings Importer"
+//
+//      1. Change the code from START to FINISH in as few moves as possible
+//
+//      2. After each challenge a lucky contestant will share their solution!
+//
+//      4. Scroll halfway down to 💡 for hints
+//
+//      3. Scroll all the way down to 📖 for a solution (not necessarily the best!)
+//
+//
+//
+//      -> -> -> recommended "Keyboard Stance"
+//      -> -> -> https://www.computerhope.com/cdn/keyboard/keyboard.jpg
+//
+//      - Ctrl:  little finger
+//      - Shift: ring finger
+//
+//      Ready?
---
+++ level1.jsx
@@ -0,0 +1,148 @@
+// ------ START ------
+
+const myData = ["apples", "oranges", "bananas"];
+
+// ------ FINISH ------
+
+const myData = [
+  {
+    fruit: "apples",
+    quantity: 1
+  },
+  {
+    fruit: "oranges",
+    quantity: 2
+  },
+  {
+    fruit: "bananas",
+    quantity: 3
+  }
+];
+
+//
+//
+//
+//
+//
+//
+//
+//
+// scroll down for 💡 hints, scroll further for 📖 a solution
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// 💡 Text Pastry extension - paste incrementing numbers like 1,2,3 on multiple cursors
+//
+//
+// 💡 Ctrl D - add next instance to selection
+// 💡 Ctrl K - remove current instance from selection
+// 💡 e.g.
+// 💡 Ctrl D, K, D       - add next-next instance to selection
+// 💡 Ctrl D, K, D, K, D - add next-next-next instance to selection
+//
+//
+// 💡 add {brackets} or "quotes" around a selection by typing only the opening bracket/quote
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// 📖
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
