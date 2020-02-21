789c23335c92a64deb454ad7efb6aed2cfdd93c0
dcorn068 <daniel@raventelemetry.com>
2020-02-21 14:15:43 -0500

    add level0

---
+++ level0.jsx
@@ -0,0 +1,21 @@
+// ------ START ------
+
+const myObject = {
+  quantity: 1,
+  fruit: "apples",
+},
+
+// ------ FINISH ------
+
+const myObject = {
+  quantity: 1,
+  fruit: "apples",
+  color: "tomato",
+},
+
+//
+//
+//
+//
+//
+// 💡 Ctrl Shift D - duplicate selection
