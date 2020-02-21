37850b70d3efb50ec8f2dc50f11f11d6843ac005
dcorn068 <daniel@raventelemetry.com>
2020-02-21 15:23:50 -0500

    add level9

---
+++ level9.jsx
@@ -0,0 +1,24 @@
+// START
+const myData = {
+  [oneProcess.id]: {
+    ...stuff
+  },
+  [twoProcess.id]: {
+    ...stuff
+  },
+  [threeProcess.id]: {
+    ...stuff
+  }
+};
+// FINISH
+const myData = {
+  [process1.id]: {
+    ...stuff
+  },
+  [process2.id]: {
+    ...stuff
+  },
+  [process3.id]: {
+    ...stuff
+  }
+};
