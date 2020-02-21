6f5bd9f38a0fb88e6fc202a5761e8186a16a9a38
dcorn068 <daniel@raventelemetry.com>
2020-02-21 10:10:24 -0500

    add level7

---
+++ level7.jsx
@@ -0,0 +1,27 @@
+// START
+import { GroupMetricsStyles } from "../GroupMetrics/GroupMetricsStyles";
+import { CenterMetricTitle } from "./CenterMetricTitle/CenterMetricTitle";
+import {
+  defaultSettingsContext,
+  SettingsContext
+} from "../../../contexts/SettingsManager/SettingsManager";
+import { ExplainerTooltipContent } from "../ExplainerTooltip";
+import { PredictedMetricTooltipContent } from "./PredictedMetricTooltip";
+import StoryRouter from "storybook-react-router";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { initialState } from "../../../providers/GlobalStateProvider";
+
+// FINISH
+import { GroupMetricsStyles } from "../../GroupMetrics/GroupMetricsStyles";
+import { CenterMetricTitle } from "./../CenterMetricTitle/CenterMetricTitle";
+import {
+  defaultSettingsContext,
+  SettingsContext
+} from "../../../../contexts/SettingsManager/SettingsManager";
+import { ExplainerTooltipContent } from "../../ExplainerTooltip";
+import { PredictedMetricTooltipContent } from "./../PredictedMetricTooltip";
+import StoryRouter from "storybook-react-router";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { initialState } from "../../../../providers/GlobalStateProvider";
